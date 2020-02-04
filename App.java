849cabecf6a0622a88dd9472f36239a8e6fb979c
h1r0t0 <shinoharahiroto0@gmail.com>
2020-02-04 17:39:13 +0900

    FileNotFound added

--- App.java
+++ App.java
@@ -2,10 +2,18 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
 
 public class App {    
       public static void main(String[] args) {
             Flag f = new Flag();
+            FileReader fr = null;
+            try{
+                  fr = new FileReader();
+            }
+            catch(Exception e){
+                  Systme.out.println("FileNotFound");
+            }
             String[] lines = getStdin();
             for (int i = 0, l = lines.length; i < l; i++) {
                     //String output = String.format("line[%s]: %s", i, lines[i]);
